d3f9ded90944ccd10dac20d1702c41de84bb36bb
BagoesBaiq <144939386+BagoesBaiq@users.noreply.github.com>
2025-07-30 14:03:11 +0700

    Update .render-build.sh

--- .render-build.sh
+++ .render-build.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
-set -o errexit
+#!/bin/bash
 
 composer install --no-dev --optimize-autoloader
+npm install
+npm run build
+php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan storage:link
+php artisan migrate --force
